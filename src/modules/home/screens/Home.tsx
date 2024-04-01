2361de13f77e7331d51c7bd4dfb28d7454af72c4
uelinton_p <uelinton.pado@gmail.com>
2024-03-31 21:32:20 -0300

    feat: insert react navigation

---
+++ src/modules/home/screens/Home.tsx
@@ -0,0 +1,10 @@
+import Text from "../../../shared/components/text/Text";
+
+const Home = ()=>{
+
+    return(
+        <Text>Home</Text>
+    )
+}
+
+export default Home;
--- src/shared/hooks/useRequest.ts
+++ src/shared/hooks/useRequest.ts
@@ -4,8 +4,10 @@ import { connectionApiPost } from '../functions/connection/connectionApi';
 import { ReturnLogin } from './returnLogin';
 import { useUserReducer } from '../../store/reducers/useReducer/useUserReducer';
 import { useGlobalReducer } from '../../store/reducers/globalReducer/useGlobalReducer';
+import { useNavigation } from '@react-navigation/native';
 
 export const useRequest = () => {
+  const { navigate } = useNavigation();
   const { setUser } = useUserReducer();
   const { setModal } = useGlobalReducer();
   const [loading, setLoading] = useState<boolean>(false);
@@ -16,6 +18,7 @@ export const useRequest = () => {
     await connectionApiPost<ReturnLogin>('http://192.168.2.23:8080/auth', body)
       .then((result) => {
         setUser(result.user);
+        navigate('Home' as never);
       })
       .catch(() => {
         setModal({
