5a90b80f077f0b25e0b49e70ca425add6ecb9191
uelinton_p <uelinton.pado@gmail.com>
2024-04-12 20:39:43 -0300

    feat: insert tab navigation menu

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,12 +1,58 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { MenuUrl } from './enums/menuUrl';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Icons } from './shared/icon/icon';
+import { theme } from './shared/themes/themes';
 import Home from './modules/home';
 import Login from './modules/login';
 import Splash from './modules/splash';
 import CreateUser from './modules/createUser';
+import Orders from './modules/Orders';
+import Profile from './modules/profile';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color }) => {
+          let iconName: string;
+
+          switch (route.name) {
+            case 'Home':
+              iconName = 'home';
+              break;
+            case 'Orders':
+              iconName = 'cart';
+              break;
+            default:
+              iconName = 'profile';
+              break;
+          }
+          // You can return any component that you like here!
+          return <Icons name={iconName} color={color} size={20} />;
+        },
+        tabBarActiveTintColor: theme.colors.mainTheme.error,
+        tabBarInactiveTintColor: theme.colors.grayTheme.gary100,
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} />
+      <Tab.Screen
+        name="Orders"
+        component={Orders}
+        options={{ title: 'Pedidos', headerShown: false }}
+      />
+      <Tab.Screen
+        name="Perfil"
+        component={Profile}
+        options={{ title: 'Perfil', headerShown: false }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const Navigation = () => {
   return (
@@ -14,8 +60,16 @@ const Navigation = () => {
       <Stack.Navigator>
         <Stack.Screen name={MenuUrl.SPLASH} component={Splash} options={{ headerShown: false }} />
         <Stack.Screen name={MenuUrl.LOGIN} component={Login} options={{ headerShown: false }} />
-        <Stack.Screen name={MenuUrl.CREATE_USER} component={CreateUser} options={{ title: MenuUrl.CREATE_USER}} />
-        <Stack.Screen name={MenuUrl.HOME} component={Home} options={{ title: 'Home' }} />
+        <Stack.Screen
+          name={MenuUrl.CREATE_USER}
+          component={CreateUser}
+          options={{ title: MenuUrl.CREATE_USER }}
+        />
+        <Stack.Screen
+          name={MenuUrl.HOME}
+          component={TabNavigation}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/modules/Orders/screens/Orders.tsx
@@ -0,0 +1,7 @@
+import Text from '../../../shared/components/text/Text';
+
+export const Orders = () => {
+  return <Text>Orders</Text>;
+};
+
+export default Orders;
